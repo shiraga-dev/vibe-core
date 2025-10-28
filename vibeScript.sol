95a91b13088741892bfa8a04ba09fa9f7e827d02
GreenWojak <113364564+Shiraga94@users.noreply.github.com>
2025-10-28 20:26:41 +0100

    Update

---
+++ vibeScript.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+import {Script} from "forge-std/Script.sol";
+pragma solidity >=0.6.2 <0.9.0;
+
+abstract contract vibeScript is Script {
+  uint internal pk = vm.envOr("PRIVATE_KEY", uint(0));
+
+  modifier broadcast() {
+    if (pk != 0) vm.startBroadcast(pk);
+    else vm.startBroadcast();
+    _;
+    vm.stopBroadcast();
+  }
+
+  function setUp() public virtual {}
+  function run() external virtual { setUp(); }
+}
