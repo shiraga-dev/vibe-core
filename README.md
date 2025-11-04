ac1fc92094e662984f01f0e4e501b33785b9e7c3
GreenWojak <113364564+Shiraga94@users.noreply.github.com>
2025-11-04 21:51:09 +0100

    Small update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # vibe-core
 
-An SDK for projects made with [vibe-cli](https://github.com/Shiraga94/vibe-cli)
+An SDK for projects made with [vibe-cli](https://github.com/Shiraga-dev/vibe-cli)
 
 > [!NOTE]
-> vibe-core is currently a work in progress, and not available on npm.
+> vibe-core is currently a work in progress.
--- Script.sol
+++ Script.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-import {Script} from "forge-std/Script.sol";
+import {Script as BaseScript} from "forge-std/Script.sol";
 pragma solidity >=0.6.2 <0.9.0;
 
-abstract contract vibeScript is Script {
+abstract contract Script is BaseScript {
   uint internal pk = vm.envOr("PRIVATE_KEY", uint(0));
 
   modifier broadcast() {
